204d0cb128792a15843d626aa47791348039520a
ujjwal04 <43586318+ujjwal04@users.noreply.github.com>
2019-09-20 12:40:18 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document.ready(function(){
-    $(.js-section-features).waypoints(function(direction){
-        if(direction=="down")
-            $(.nav).addClass("sticky");
-        else
-            $(.nav).removeClass("sticky");
-
-    }
-)}))
